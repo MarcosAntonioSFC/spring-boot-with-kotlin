a8529e8737f485407ed128187247f415572ef121
Marcos Filho <marcos.silva@tinnova.com.br>
2023-07-06 15:11:13 -0300

    Commiting

---
+++ src/main/kotlin/br/com/learn/Startup.kt
@@ -0,0 +1,11 @@
+package br.com.learn
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Startup
+
+fun main(args: Array<String>) {
+    runApplication<Startup>(*args)
+}
