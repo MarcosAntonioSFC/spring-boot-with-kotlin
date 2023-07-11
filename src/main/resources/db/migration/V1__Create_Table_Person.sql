0fd231fdc6d2e765b3f0ad4aa8a2c0cb323d9f45
Marcos Filho <marcos.silva@tinnova.com.br>
2023-07-11 10:05:46 -0300

    Adding migration at project

---
+++ src/main/resources/db/migration/V1__Create_Table_Person.sql
@@ -0,0 +1,9 @@
+create table my_database.person
+(
+    id         bigint auto_increment
+        primary key,
+    address    varchar(100) not null,
+    first_name varchar(80)  not null,
+    gender     varchar(6)   not null,
+    last_name  varchar(80)  not null
+);
