a22113100735ad0c1a8ac140edfe8a36bcb7c48f
Marcos Filho <marcos.silva@tinnova.com.br>
2023-07-11 10:49:40 -0300

    Adding content negociation

--- src/main/kotlin/br/com/learn/controller/PersonController.kt
+++ src/main/kotlin/br/com/learn/controller/PersonController.kt
@@ -1,9 +1,11 @@
 package br.com.learn.controller
 
 import br.com.learn.services.PersonService
+import br.com.learn.util.MediaType.APPLICATION_JSON_VALUE
+import br.com.learn.util.MediaType.APPLICATION_XML_VALUE
+import br.com.learn.util.MediaType.APPLICATION_X_YAML_VALUE
 import br.com.learn.vo.v1.PersonVO
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
@@ -14,27 +16,39 @@ class PersonController {
     @Autowired
     private lateinit var service: PersonService;
 
-    @GetMapping(produces = [MediaType.APPLICATION_JSON_VALUE])
+    @GetMapping(
+            produces = [APPLICATION_JSON_VALUE, APPLICATION_XML_VALUE, APPLICATION_X_YAML_VALUE]
+    )
     fun findAll(): List<PersonVO> {
         return service.findAll();
     }
 
-    @GetMapping("/{id}", produces = [MediaType.APPLICATION_JSON_VALUE])
+    @GetMapping("/{id}",
+            produces = [APPLICATION_JSON_VALUE, APPLICATION_XML_VALUE, APPLICATION_X_YAML_VALUE]
+    )
     fun findById(@PathVariable("id") id: Long): PersonVO {
         return service.findById(id);
     }
 
-    @PostMapping(produces = [MediaType.APPLICATION_JSON_VALUE])
+    @PostMapping(
+            consumes = [APPLICATION_JSON_VALUE, APPLICATION_XML_VALUE, APPLICATION_X_YAML_VALUE],
+            produces = [APPLICATION_JSON_VALUE, APPLICATION_XML_VALUE, APPLICATION_X_YAML_VALUE]
+    )
     fun create(@RequestBody person: PersonVO): PersonVO {
         return service.create(person);
     }
 
-    @PutMapping(produces = [MediaType.APPLICATION_JSON_VALUE])
+    @PutMapping(
+            consumes = [APPLICATION_JSON_VALUE, APPLICATION_XML_VALUE, APPLICATION_X_YAML_VALUE],
+            produces = [APPLICATION_JSON_VALUE, APPLICATION_XML_VALUE, APPLICATION_X_YAML_VALUE]
+    )
     fun update(@RequestBody person: PersonVO): PersonVO {
         return service.update(person);
     }
 
-    @DeleteMapping(path = ["/{id}"], produces = [MediaType.APPLICATION_JSON_VALUE])
+    @DeleteMapping(path = ["/{id}"],
+            consumes = [APPLICATION_JSON_VALUE, APPLICATION_XML_VALUE, APPLICATION_X_YAML_VALUE],
+            produces = [APPLICATION_JSON_VALUE, APPLICATION_XML_VALUE, APPLICATION_X_YAML_VALUE])
     fun delete(@PathVariable("id") id: Long): ResponseEntity<Any> {
         service.delete(id);
         return ResponseEntity.noContent().build();
---
+++ src/main/kotlin/br/com/learn/util/MediaType.kt
@@ -0,0 +1,11 @@
+package br.com.learn.util
+
+import org.springframework.http.MediaType
+
+object MediaType {
+
+    const val APPLICATION_JSON_VALUE = "application/json";
+    const val APPLICATION_XML_VALUE = "application/xml";
+    const val APPLICATION_X_YAML_VALUE = "application/x-yaml"
+
+}
